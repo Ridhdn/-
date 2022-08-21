b780024d0c95ba8f85776416dfc4e79bc02b7873
Ridhdn <109890458+Ridhdn@users.noreply.github.com>
2022-08-22 00:51:21 +0900

    Add files via upload

---
+++ ssgwebedu_2022/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>로그인</title>
+</head>
+<body>
+    
+    <form action="login_server.php" method="post">
+    <h2>로그인</h2>
+
+    <?php if(isset($_GET['error'])){ ?>
+    <p><?php echo $_GET['error']; ?></p>
+    <?php } ?>
+
+    <label>아이디</label>
+    <input type="text" placeholder="아이디..." name="user_id">
+    <br>
+
+    <label>비밀번호</label>
+    <input type="password" placeholder="비밀번호..." name="pass1">
+    <br>
+
+    <button type="submit">로그인</button>
+    <br>
+    <a href="register_view.php">회원가입 페이지</a>
+
+    </form>
+
+</body>
+</html>
